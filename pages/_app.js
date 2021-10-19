0438ad87fcf748262a90dfe752c8f490c6443fd1
Agmar Putra <agmar.putra@spesolution.net>
2021-10-19 15:03:00 +0700

    menambahkan nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import '../styles/nprogress.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeError", NProgress.done);
+Router.events.on("routeChangeComplete", NProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return (
